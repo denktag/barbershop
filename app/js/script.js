e980d4244d8ef1fa309b4fa4595a09fe16fead6b
denktag <1deniskri@mail.ru>
2021-03-16 15:04:22 +0300

    added menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,16 @@
 
+$(document).on("click", ".header__item-link", function(e) {
+	e.preventDefault();
+	let id  = $(this).attr('href');
+	let top = $(id).offset().top; // получаем координаты блока
+	$('body, html').animate({scrollTop: top}, 800); // плавно переходим к блоку
+});
+
+$(document).on("click", ".scroll-up", function(e) {
+	e.preventDefault();
+	$('body, html').animate({scrollTop: 0}, 800);
+});
+
 new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.button-next',
